5140a5905e2f2e6e58cf3d075180d554c63f11b0
Pa Her <pa.her@gofmx.com>
2022-01-21 18:05:39 -0600

    Add files

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+  <Hero />
+  <LargeCardDisplay 
+  v-for="cardInfo in largeCardInfo" 
+  :key="cardInfo.id"
+  :cardSection="cardInfo"
+  />
+</div>
+
+</template>
+
+<script>
+import {largeCardSections} from "@/assets/data.js"
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
