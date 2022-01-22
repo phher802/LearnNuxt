f27f7101e3387968d1f390bab48676898780aec0
Pa Her <pa.her@gofmx.com>
2022-01-21 23:44:37 -0600

    Add nav bar to components

---
+++ components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="/">Rent A Fire Extinguisher</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a href="/products" class="nav-link"
+            >Products <span class="sr-only">(current)</span></a
+          >
+        </li>
+        <li class="nav-item">
+          <a href="/my-items" class="nav-link">My Items <span class="sr-only">(current)</span></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,35 @@
 <template>
-<div class="container">
-  <Hero />
-  <LargeCardDisplay 
-  v-for="cardInfo in largeCardInfo" 
-  :key="cardInfo.id"
-  :cardSection="cardInfo"
-  />
-  <SmallCardDisplay 
-  v-for="cardInfo in smallCardSections"
-  :key="cardInfo.id"
-  :cardSection="cardInfo"
-  />
-</div>
-
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-
-import {largeCardSections, smallCardSections } from "@/assets/data.js"
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+import Nav from "../components/Nav.vue";
 
 export default {
   data() {
     return {
       largeCardInfo: largeCardSections,
-      smallCardSections
-    }
-  }
-}
+      smallCardSections,
+    };
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
